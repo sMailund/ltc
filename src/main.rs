ab219c1cf1235558da0694a5a106fbfab0fe7721
Simen Mailund Svendsen <simen@erus.no>
2024-02-22 10:18:59 +0100

    print results

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use chrono::{Duration, NaiveDateTime};
-use itertools::Itertools;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::path::Path;
+
+use chrono::NaiveDateTime;
 
 fn main() {
     // Get the file path from the command line arguments
@@ -16,7 +15,6 @@ fn main() {
 
     // Read the file and parse dates
     read_dates(file_path);
-
 }
 
 fn read_dates(file_path: &str) {
@@ -30,7 +28,8 @@ fn read_dates(file_path: &str) {
                 let mut split = trimmed.split(",");
                 let hash = split.next().expect("could not find first element");
                 let date = split.next().expect("could not find second element");
-                let date = NaiveDateTime::parse_from_str(date, "%a %b %e %H:%M:%S %Y %z").expect("could not parse date");
+                let date = NaiveDateTime::parse_from_str(date, "%a %b %e %H:%M:%S %Y %z")
+                    .expect("could not parse date");
                 let now = chrono::offset::Utc::now().naive_utc();
                 let days_ago = now.signed_duration_since(date).num_days();
                 Some((hash.to_string(), days_ago))
@@ -38,30 +37,8 @@ fn read_dates(file_path: &str) {
         })
         .collect();
 
+    dates.iter().for_each(|item| {
+        println!("{},{}", item.0, item.1);
+    })
 }
 
-fn days_difference(date: NaiveDateTime) -> i64 {
-    let now = chrono::offset::Utc::now().naive_utc();
-    let duration = now.signed_duration_since(date);
-    duration.num_days()
-}
-
-fn days_difference_with_hash(date: NaiveDateTime, hash: &str) -> (&str, i64) {
-    let now = chrono::offset::Utc::now().naive_utc();
-    let duration = now.signed_duration_since(date);
-    (hash, duration.num_days())
-}
-
-fn calculate_median(values: &Vec<i64>) -> i64 {
-    let mut sorted_values = values.clone();
-    sorted_values.sort();
-    let n = sorted_values.len();
-    println!("len: {}", n);
-
-    if n % 2 == 1 {
-        sorted_values[n / 2]
-    } else {
-        let mid = n / 2;
-        (sorted_values[mid - 1] + sorted_values[mid]) / 2
-    }
-}
